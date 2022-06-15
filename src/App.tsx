e7bcbf658db3e842e5a1c3f491a56c20d0126f29
hakimm77 <hakimboum406@gmail.com>
2022-06-15 23:57:39 +0100

    added the server folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [url, setUrl] = useState("")
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    const queryInfo = {active: true, lastFocusedWindow: true};
+    const queryInfo = { active: true, lastFocusedWindow: true };
 
-    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-      const url = tabs[0].url;
-      setUrl(url ?? "");
-    });
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, (tabs) => {
+        const url = tabs[0].url;
+        setUrl(url ?? "");
+      });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
         <p>URL:</p>
-        <p>
-          {url}
-        </p>
+        <p>{url}</p>
       </header>
     </div>
   );
 };
 
-export default App
+export default App;
