187f7279d6b04101753383c95b643b6cf3565161
hakimm77 <hakimboum406@gmail.com>
2022-06-17 19:00:27 +0100

    worked on mutual list of followings

--- server/controllers/apiIntegration.controller.js
+++ server/controllers/apiIntegration.controller.js
@@ -5,11 +5,49 @@ const { TwitterApi } = require("twitter-api-v2");
 const twitterClient = new TwitterApi(process.env.BEARER_TOKEN);
 const roClient = twitterClient.readOnly;
 
-const getMutualFollowingsList = async (req, res) => {
-  const user = await roClient.v2.usersByUsernames("elonmusk");
+let userNextPage = "";
+let targetNextPage = "";
+let mutuals = [];
 
-  const userFollowers = await twitterClient.v2.followers(user.data[0].id);
-  res.json(userFollowers);
+const getMutualFollowingsList = async (req, res) => {
+  const user = await roClient.v2.usersByUsernames("GabrielPeterss4");
+  const target = await roClient.v2.usersByUsernames("hakim_hamaili");
+
+  const userFollowers = userNextPage
+    ? await roClient.v2.followers(user.data[0].id, {
+        pagination_token: userNextPage,
+      })
+    : await roClient.v2.followers(user.data[0].id);
+
+  const targetFollowings = targetNextPage
+    ? await roClient.v2.following(target.data[0].id, {
+        pagination_token: targetNextPage,
+      })
+    : await roClient.v2.following(target.data[0].id);
+
+  const filteredList = userFollowers.data.filter((element) =>
+    targetFollowings.data.includes(element)
+  );
+
+  filteredList.forEach((userElement) => {
+    mutuals = [...mutuals, userElement];
+  });
+
+  if (userFollowers.meta.next_token || targetFollowings.meta.next_token) {
+    console.log("Still searching");
+    userNextPage = userFollowers.meta.next_token
+      ? userFollowers.meta.next_token
+      : "";
+    targetNextPage = targetFollowings.meta.next_token
+      ? targetFollowings.meta.next_token
+      : "";
+
+    getMutualFollowingsList(req, res);
+  } else {
+    console.log("finished! check results");
+
+    res.json(mutuals);
+  }
 };
 
 module.exports = { getMutualFollowingsList };
