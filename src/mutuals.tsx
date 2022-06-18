cf954d3d6d85d854cc30cf62452f2d5baf4131a3
Alexander Törnvall <alextorn.python@gmail.com>
2022-06-18 19:22:13 +0200

    restored mutuals + changed default title param

--- src/mutuals.tsx
+++ src/mutuals.tsx
@@ -1,85 +1,76 @@
-import React, { useEffect, useState } from "react";
-import { Mutual } from "./mutual";
-import { getUsernameFromUrl } from "./utils";
-import { IMutual } from "./types";
-import axios from "axios";
+import React, {useEffect, useState} from "react"
+import {Mutual} from "./mutual";
+import {getUsernameFromUrl} from "./utils";
+import {IMutual} from "./types";
 
 function Mutuals() {
-  const [data, setData] = useState<IMutual[] | []>([]);
-  const [url, setUrl] = useState("");
-  const [username, setUsername] = useState("");
-  const [page, setPage] = useState(1);
-  const [status, setStatus] = useState("idle");
-  const [message, setMessage] = useState("");
+  const [data, setData] = useState<IMutual[] | []>([])
+  const [url, setUrl] = useState("")
+  const [username, setUsername] = useState("")
+  const [page, setPage] = useState(1)
+  const [status, setStatus] = useState("idle")
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
-    const queryInfo = { active: true, lastFocusedWindow: true };
+    const queryInfo = {active: true, lastFocusedWindow: true};
 
-    chrome.tabs &&
-      chrome.tabs.query(queryInfo, (tabs) => {
-        const chromeUrl = tabs[0].url;
-        setUrl(chromeUrl ?? "");
-      });
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const chromeUrl = tabs[0].url;
+      setUrl(chromeUrl ?? "");
+    });
   }, []);
 
   useEffect(() => {
-    const twitterUsername = getUsernameFromUrl(url);
+    const twitterUsername = getUsernameFromUrl(url)
     if (twitterUsername) {
-      setUsername(twitterUsername);
+      setUsername(twitterUsername)
     }
-  }, [url]);
+  }, [url])
 
   useEffect(() => {
-    async function fetchMutuals(
-      targetUsername: string,
-      pageNumber = 1
-    ): Promise<IMutual[]> {
-      const res = await axios.get(
-        `https://fast-forest-05025.herokuapp.com/mutual?target_username=${targetUsername}&page=${pageNumber}`
-      );
-      if (res.status !== 200) {
-        setStatus(`error: ${res.status}`);
-        return [];
+    async function fetchMutuals(targetUsername: string, pageNumber = 1): Promise<IMutual[]> {
+      const res = await fetch(`${process.env.REACT_APP_BASE_URL}/mutual/?target_username=${targetUsername}&page=${pageNumber}`)
+      if (!res.ok) {
+        setStatus(`error: ${res.status}`)
+        return []
       }
-      const textResponse = await res.data;
+      const textResponse = await res.text()
       if (!textResponse) {
-        setMessage(
-          "empty response: either the account is not supported or the accounts don't have any mutual followers"
-        );
-        return [];
+        setMessage("empty response: either the account is not supported or the accounts don't have any mutual followers")
+        return []
       }
-      return await textResponse;
+      return await JSON.parse(textResponse)
     }
 
     if (username) {
-      setStatus("loading");
-      setMessage("");
-      fetchMutuals(username, page)
-        .then((resData) => {
-          setData(resData);
-          setStatus(`idle`);
-        })
-        .catch((err) => {
-          setStatus(`error: ${err}`);
-        });
+      setStatus("loading")
+      setMessage("")
+      fetchMutuals(username, page).then(resData => {
+        setData(resData)
+        setStatus(`idle`)
+      }).catch(err => {
+        setStatus(`error: ${err}`)
+      })
     }
-  }, [username, page]);
+  }, [username, page])
   return (
     <div>
       <p className="text-lg mb-2">
-        <strong>request status:</strong> {status}
+        <strong>status:</strong> {status}
       </p>
       {message && (
         <p className="text-lg mb-2">
-          <strong>request info:</strong> {message}
+          <strong>info:</strong> {message}
         </p>
       )}
       {data?.length > 0 && (
         <div>
           <div className="grid grid-cols-1 gap-4 mb-3">
-            {data.map((mutual) => (
-              <Mutual key={mutual.link} {...mutual} />
-            ))}
+            {data.map(mutual => {
+              return (
+                <Mutual key={mutual.link} {...mutual} />
+              )
+            })}
           </div>
           <div className="flex justify-between items-center">
             <button
@@ -98,7 +89,7 @@ function Mutuals() {
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export { Mutuals };
+export {Mutuals}
