38515f8108405320eeb729ec1e7fbe1c3f712361
hakimm77 <hakimboum406@gmail.com>
2022-06-18 14:28:49 +0100

    deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const {
-  getMutual,
-} = require("./controllers/apiIntegration.controller");
+const { getMutual } = require("./controllers/apiIntegration.controller");
+const { env } = require("process");
 
 const app = express();
-app.use(cors())
-const PORT = 4000;
+app.use(cors());
+const PORT = process.env.PORT || 4000;
 
 app.get("/mutual", getMutual);
 
