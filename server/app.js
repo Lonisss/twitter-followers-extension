725b8a81ac6897ec1fb7523d6f9a8732046bcd9a
Alexander Törnvall <alextorn.python@gmail.com>
2022-06-18 13:47:51 +0200

    frontend showing statuses

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const {
   getMutual,
 } = require("./controllers/apiIntegration.controller");
 
 const app = express();
+app.use(cors())
 const PORT = 4000;
 
 app.get("/mutual", getMutual);
--- src/mutuals.tsx
+++ src/mutuals.tsx
@@ -3,14 +3,14 @@ import {Mutual} from "./mutual";
 import {getUsernameFromUrl} from "./utils";
 import {IMutual} from "./types";
 
-const baseUrl = "http://localhost:4000/"
-
+const baseUrl = "http://localhost:4000"
 
 function Mutuals() {
   const [data, setData] = useState<IMutual[] | []>([])
   const [url, setUrl] = useState("")
   const [username, setUsername] = useState("")
   const [page, setPage] = useState(1)
+  const [status, setStatus] = useState("loading")
 
   useEffect(() => {
     const queryInfo = {active: true, lastFocusedWindow: true};
@@ -19,7 +19,7 @@ function Mutuals() {
       const chromeUrl = tabs[0].url;
       setUrl(chromeUrl ?? "");
     });
-    setUrl("https://twitter.com/citydao")
+    // setUrl("https://twitter.com/citydao")
   }, []);
 
   useEffect(() => {
@@ -31,21 +31,32 @@ function Mutuals() {
 
   useEffect(() => {
     async function fetchMutuals(targetUsername: string, pageNumber = 1): Promise<IMutual[]> {
-      const res = await fetch(`${baseUrl}?target_username=${targetUsername}&page=${pageNumber}`)
+      const res = await fetch(`${baseUrl}/mutual/?target_username=${targetUsername}&page=${pageNumber}`)
       if (!res.ok) {
+        setStatus(`error ${res.status}`)
         return []
       }
       return await res.json()
     }
 
     if (username) {
-      fetchMutuals(username, page).then((resData) => {
+      setStatus("loading")
+      fetchMutuals(username, page).then(resData => {
         setData(resData)
+        setStatus("idle")
+      }).catch(err => {
+        setStatus(`error: ${err}`)
       })
     }
   }, [username, page])
   return (
     <div>
+      <p className="text-lg mb-4">
+        Current URL: {url}
+      </p>
+      <p className="text-lg mb-4">
+        Current status: {status}
+      </p>
       {data && (
         <div className="grid grid-cols-1 gap-4">
           {data.map(mutual => {
@@ -55,11 +66,15 @@ function Mutuals() {
           })}
         </div>
       )}
-      <p>
-        testing: {url}
-      </p>
-      <button onClick={() => setPage(page + 1)}>
+      <button
+        onClick={() => setPage(page + 1)}
+      >
         Increment page
+      </button>
+      <button
+        onClick={() => setPage(page - 1)}
+      >
+        Decrement page
       </button>
     </div>
   )
