ebc582f1e7bd0bb5284068464e2508390fd27da9
hakimm77 <hakimboum406@gmail.com>
2022-06-29 17:29:24 +0100

    added feature of retiriving pfp for common users

--- src/components/mutual.tsx
+++ src/components/mutual.tsx
@@ -1,27 +1,42 @@
-import React from "react"
-import {IMutual} from "../types";
-import {ExternalLinkIcon} from "@heroicons/react/outline";
-import {openInNewTab} from "../utils";
+import React from "react";
+import { IMutual } from "../types";
+import { ExternalLinkIcon } from "@heroicons/react/outline";
+import { openInNewTab } from "../utils";
 
-function Mutual({username, name, link}: IMutual) {
+function Mutual({ username, name, link }: IMutual) {
   return (
     <div
       onClick={() => openInNewTab(link)}
       className="flex w-full border border-gray-500 p-2 hover:bg-gray-700 rounded-md transition-colors justify-between cursor-pointer select-none"
     >
       <div>
-        <h3 className="text-xl font-medium">
-          {name} ({username})
-        </h3>
-        <p className="text-lg text-gray-200">
-          {link}
-        </p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={`https://unavatar.io/twitter/${username}`}
+            style={{
+              borderRadius: "100%",
+              margin: 5,
+              width: "40px",
+              height: "40px",
+            }}
+          />
+          <h3 className="text-xl font-medium">
+            {name} ({username})
+          </h3>
+        </div>
+        <p className="text-lg text-gray-200">{link}</p>
       </div>
       <div>
         <ExternalLinkIcon className="w-4 text-gray-50" strokeWidth={1} />
       </div>
     </div>
-  )
+  );
 }
 
-export {Mutual}
+export { Mutual };
