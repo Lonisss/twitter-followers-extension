f7db121ec2febb3b27081dc42e81d65c855c1d4b
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-29 17:16:58 +0200

    chore(changes): added back changes from earlier

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const { getMutual } = require("./controllers/apiIntegration.controller");
-const { env } = require("process");
 
-const app = express();
-app.use(cors());
+const server = express();
+server.use(cors());
 const PORT = process.env.PORT || 4000;
 
-app.get("/mutual", getMutual);
+server.get("/mutual", getMutual);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server up on port ${PORT}`);
 });
